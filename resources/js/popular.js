017dd9e24c6a011904e809a33f150fca68fd23f9
gusleaooliveira <gus.leaono@gmail.com>
2020-06-14 18:01:47 -0300

    alteração feita em:14/06/2020-18:01:47

---
+++ resources/js/popular.js
@@ -0,0 +1,25 @@
+$(window).on("load", () => {
+    var largura = window.innerWidth;
+    var altura = window.innerHeight;
+    var url = "";
+        
+    if(largura >= 0 && largura <= 600){
+        url = "https://api-mrconstrucoesapp.herokuapp.com/imagens/pequeno";
+    }    
+    else if (largura >= 601 && largura <= 768){
+        url = "https://api-mrconstrucoesapp.herokuapp.com/imagens/medio";
+    }
+    else if (largura >= 769 && largura <= 992){
+        url = "https://api-mrconstrucoesapp.herokuapp.com/imagens/grande";        
+    }
+    else{
+        url = "https://api-mrconstrucoesapp.herokuapp.com/imagens/grande";        
+    }
+
+
+    $.ajax({ url: url , success: (resposta)=> {
+        let lista  = resposta;
+
+        
+    }});        
+});
